ed18d2c91c06673855efb437f7ae3580ae95db5d
Suraj Ganguly <surajganguly18@gmail.com>
2021-08-26 12:41:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const App = () => {
         renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
 
         onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
-////
+
       />
       
 
